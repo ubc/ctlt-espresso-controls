75cef619b68221540d33f8c3b81d4702d514f6aa
psmagicman <julienlaw1@gmail.com>
2013-08-01 10:15:22 -0700

    room setup pictures added

--- lib/class.ctlt-espresso-room-setup.php
+++ lib/class.ctlt-espresso-room-setup.php
@@ -1,5 +1,7 @@
 <?php
 
+// TODO: style the metabox content
+
 class CTLT_Espresso_Room_Setup extends CTLT_Espresso_Metaboxes {
 	
 	public function __construct() {
@@ -8,7 +10,7 @@ class CTLT_Espresso_Room_Setup extends CTLT_Espresso_Metaboxes {
 	}
 
 	public function init_assets() {
-		
+
 	}
 
 	public function room_setup_metabox() {
@@ -28,7 +30,48 @@ class CTLT_Espresso_Room_Setup extends CTLT_Espresso_Metaboxes {
 
 	public function rooms() {
 		?>
-		
+		<div class="ctlt-espresso-row">
+			<div class="room-setup">
+				<img src="<?php echo CTLT_ESPRESSO_CONTROLS_ASSETS_URL . 'hollow-sqaure.png';?>" alt="Hollow Square" />
+				<input type="radio" name="room-setup-radios" id="room-setup-radios-1" value="Hollow Sqaure"> Hollow Sqaure
+			</div>
+			<div class="room-setup">
+				<img src="<?php echo CTLT_ESPRESSO_CONTROLS_ASSETS_URL . 'classroom-style.png';?>" alt="Classroom Style" />
+				<input type="radio" name="room-setup-radios" id="room-setup-radios-2" value="Classroom Style"> Classroom Style
+			</div>
+			<div class="room-setup">
+				<img src="<?php echo CTLT_ESPRESSO_CONTROLS_ASSETS_URL . 'conference-style.png';?>" alt="Conference Style" />
+				<input type="radio" name="room-setup-radios" id="room-setup-radios-3" value="Conference Style"> Conference Style
+			</div>
+		</div>
+		<div class="ctlt-espresso-row">
+			<div class="room-setup">
+				<img src="<?php echo CTLT_ESPRESSO_CONTROLS_ASSETS_URL . 'circle-of-chairs.png';?>" alt="Circle of Chairs" />
+				<input type="radio" name="room-setup-radios" id="room-setup-radios-4" value="Circle of Chairs"> Circle of Chairs
+			</div>
+			<div class="room-setup">
+				<img src="<?php echo CTLT_ESPRESSO_CONTROLS_ASSETS_URL . 'theater-style.png';?>" alt="Theater Style" />
+				<input type="radio" name="room-setup-radios" id="room-setup-radios-5" value="Theater Style"> Theater Style
+			</div>
+			<div class="room-setup">
+				<img src="<?php echo CTLT_ESPRESSO_CONTROLS_ASSETS_URL . 'u-shaped-style.png';?>" alt="U-Shaped Style" />
+				<input type="radio" name="room-setup-radios" id="room-setup-radios-6" value="U-Shaped Style"> U-Shaped Style
+			</div>
+		</div>
+		<div class="ctlt-espresso-row">
+			<div class="room-setup">
+				<img src="<?php echo CTLT_ESPRESSO_CONTROLS_ASSETS_URL . 'seminar-style.png';?>" alt="Seminar Style" />
+				<input type="radio" name="room-setup-radios" id="room-setup-radios-7" value="Seminar Style"> Seminar Style
+			</div>
+			<div class="room-setup">
+				<img src="" alt="Open Space" />
+				<input type="radio" name="room-setup-radios" id="room-setup-radios-8" value="Open Space"> Open Space
+			</div>
+			<div class="room-setup">
+				<img src="" alt="Custom Arrangement" />
+				<input type="radio" name="room-setup-radios" id="room-setup-radios-9" value="Custom Arrangement"> Custom Arrangement
+			</div>
+		</div>
 		<?php
 	}
 

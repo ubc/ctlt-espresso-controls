5ee130f13da302ec5eed69850d5b15f75cced7b5
psmagicman <julienlaw1@gmail.com>
2013-08-01 11:13:36 -0700

    renamed the css class tag

--- lib/class.ctlt-espresso-additional-information.php
+++ lib/class.ctlt-espresso-additional-information.php
@@ -25,52 +25,52 @@ class CTLT_Espresso_Additional_Information extends CTLT_Espresso_Metaboxes{
 	public function render_additional_information() {
 		?>
 		<div class="ctlt-espresso-row">
-			<div class="room-setup">
+			<div class="">
 				<label>Room Setup Notes:</label>
 			</div>
-			<div class="room-setup">
+			<div class="">
 				<textarea></textarea>
 			</div>
-			<div class="room-setup">
+			<div class="">
 				<label>Admin Support Notes</label>
 			</div>
-			<div class="room-setup">
+			<div class="">
 				<textarea></textarea>
 			</div>
 		</div>
 		<div class="ctlt-espresso-row">
-			<div class="room-setup">
+			<div class="">
 				<label>A/V and Computer Requirements:</label>
 			</div>
-			<div class="room-setup">
+			<div class="">
 				<textarea></textarea>
 			</div>
-			<div class="room-setup">
+			<div class="">
 				<label>Marketing & Communication Support Notes:</label>
 			</div>
-			<div class="room-setup">
+			<div class="">
 				<textarea></textarea>
 			</div>
 		</div>
 		<div class="ctlt-espresso-row">
-			<div class="room-setup">
+			<div class="">
 				<label>Catering Notes:</label>
 			</div>
-			<div class="room-setup">
+			<div class="">
 				<textarea></textarea>
 			</div>
 		</div>
 		<div class="ctlt-espresso-row">
-			<div class="room-setup">
+			<div class="">
 				<label>Need Help Setting Up the Room?</label>
 			</div>
-			<div class="room-setup">
+			<div class="">
 				<input type="checkbox" id="help-setup-room" />
 			</div>
-			<div class="room-setup">
+			<div class="">
 				<label>I Would Like Signs Posted for My Event:</label>
 			</div>
-			<div class="room-setup">
+			<div class="">
 				<input type="checkbox" id="post-signs" />
 			</div>
 		</div>

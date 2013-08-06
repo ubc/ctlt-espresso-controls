6b49b3703c554316d3a1745c3c8680a00619030b
psmagicman <julienlaw1@gmail.com>
2013-08-06 10:34:15 -0700

    saving is now working for costs fields

--- lib/class.ctlt-espresso-costs.php
+++ lib/class.ctlt-espresso-costs.php
@@ -2,13 +2,28 @@
 
 class CTLT_Espresso_Costs extends CTLT_Espresso_Metaboxes {
 	
+	static $costs_arr = null;
 
 	public function __construct() {
+		$this->init_default_assets();
 		add_action( 'add_meta_boxes_' . $this->espresso_slug, array( $this, 'costs_metabox' ) );
+		add_action( 'save_post', array( $this, 'save' ) );
 	}
 
-	public function init_assets() {
-
+	public function init_default_assets() {
+		self::$costs_arr = array(
+			'name' => 'Costs',
+			'id' => $this->prefix . 'costs',
+			'type' => 'text',
+			'options' => array(
+				array( 'name' => 'Facilitator Pay (Total)', 'id' => 'facilitator-pay' ),
+				array( 'name' => 'TA Pay (Total)', 'id' => 'ta-pay' ),
+				array( 'name' => 'Room Cost', 'id' => 'room-cost' ),
+				array( 'name' => 'Ad Cost', 'id' => 'ad-cost' ),
+				array( 'name' => 'Food Cost', 'id' => 'food-cost' ),
+				array( 'name' => 'Other Cost', 'id' => 'other-cost' )
+			)
+		);
 	}
 
 	public function costs_metabox() {
@@ -23,69 +38,60 @@ class CTLT_Espresso_Costs extends CTLT_Espresso_Metaboxes {
 	}
 
 	public function render_costs() {
-		?>
-		<div class="ctlt-events-row">
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<label>Facilitator Pay (Total)</label>
-			</div>
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<div class="input-prepend">
-					<span class="add-on">$</span>
-					<input class="ctlt-colspan-5 ctlt-events-col" id="facilitator-cost" type="text">
-				</div>
-			</div>
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<label>TA Pay (Total)</label>
-			</div>
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<div class="input-prepend">
-					<span class="add-on">$</span>
-					<input class="ctlt-colspan-5 ctlt-events-col" id="ta-cost" type="text">
-				</div>
-			</div>
-		</div>
-		<div class="ctlt-events-row">
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<label>Room Cost</label>
-			</div>
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<div class="input-prepend">
-					<span class="add-on">$</span>
-					<input class="ctlt-colspan-5 ctlt-events-col" id="room-cost" type="text">
-				</div>
-			</div>
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<label>Ad Cost</label>
-			</div>
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<div class="input-prepend">
-					<span class="add-on">$</span>
-					<input class="ctlt-colspan-5 ctlt-events-col" id="ad-cost" type="text">
-				</div>
-			</div>
-		</div>
-		<div class="ctlt-events-row">
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<label>Food Cost</label>
-			</div>
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<div class="input-prepend">
-					<span class="add-on">$</span>
-					<input class="ctlt-colspan-5 ctlt-events-col" id="food-cost" type="text">
-				</div>
-			</div>
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<label>Other Cost</label>
-			</div>
-			<div class="ctlt-colspan-3 ctlt-events-col">
-				<div class="input-prepend">
-					<span class="add-on">$</span>
-					<input class="ctlt-colspan-5 ctlt-events-col" id="other-cost" type="text">
-				</div>
-			</div>
-		</div>
-		<?php
+		global $post;
+		echo '<input type="hidden" name="' . $this->prefix . 'costs_noncename" value="' . wp_create_nonce( CTLT_ESPRESSO_CONTROLS_BASENAME ) . '" />';
+		$meta = get_post_custom( $post->ID );
+		//var_dump( $meta );
+		$this->costs( $meta );
 	}
+
+	public function costs( $meta ) {
+		//$count = count( self::$costs_arr['options'] ) * 2;
+		foreach( self::$costs_arr['options'] as $option ) { 
+			$value = isset( $meta[$option['id']] ) ? esc_attr( $meta[$option['id']][0] ) : '';
+			//echo $count % 4 === 0 ? '<div class="ctlt-events-row">' : ''; ?>
+			<div class="ctlt-events-row">
+				<div class="ctlt-colspan-3 ctlt-events-col">
+					<label for="<?php echo $option['id']; ?>"><?php echo $option['name']; ?></label>
+				</div>
+				<div class="ctlt-colspan-3 ctlt-events-col currency-prepend">
+					<!--<span class="currency">$</span>-->
+					<input name="<?php echo $option['id']; ?>" id="<?php echo $option['id']; ?>" type="text" value="<?php echo $value; ?>">
+				</div>
+			</div>
+			<?php //echo $count % 4 === 1 ? '</div>' : '';
+			$count -= 1;
+		}
+	}
+
+	public function save( $post_id ) {
+		
+		// verify the nonce
+		if( !wp_verify_nonce($_POST[$this->prefix .'costs_noncename'], CTLT_ESPRESSO_CONTROLS_BASENAME) ) {
+			return $post_id;
+		}
+		// check autosave
+		if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return $post_id;
+		}
+		// check permissions
+		if( 'page' == $_POST['post_type'] ) {
+			if( !current_user_can( 'edit_page', $post_id ) ) {
+				return $post_id;
+			}
+		}
+		elseif( !current_user_can( 'edit_post', $post_id ) ) {
+			return $post_id;
+		}
+		// do the actual saving here
+		foreach( self::$costs_arr['options'] as $option ) {
+			if( isset( $_POST[$option['id']] ) ) {
+				update_post_meta( $post_id, $option['id'], $_POST[$option['id']] );
+			}
+		}
+		
+	}
+
 }
 
 new CTLT_Espresso_Costs();

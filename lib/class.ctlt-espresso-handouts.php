e7a922cd73a4c8fc45c5535d078034ae62c8afd0
psmagicman <julienlaw1@gmail.com>
2013-08-06 13:09:37 -0700

    uploading files work properly now

--- lib/class.ctlt-espresso-handouts.php
+++ lib/class.ctlt-espresso-handouts.php
@@ -3,15 +3,15 @@
 class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 	
 	static $radios_arr = null;
+	static $handout_file = null;
 
 	public function __construct() {
 		//add_action( 'plugins_loaded', array( $this, 'init_handouts_properties') );
 
 		$this->init_handouts_properties();
 		add_action( 'add_meta_boxes_' . $this->espresso_slug, array( $this, 'handouts_metabox' ) );
-		//add_action( 'add_meta_boxes', array( $this, 'handouts_metabox' ) );
 		add_action( 'save_post', array( $this, 'save' ) );
-		//var_dump( self::$radios_arr );
+		add_action( 'post_edit_form_tag', array( $this, 'update_edit_form' ) );
 	}
 
 	public function init_handouts_properties() {
@@ -26,6 +26,11 @@ class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 				array( 'name' => 'Received', 'value' => 'received' ),
 				array( 'name' => 'Copying Complete', 'value' => 'copying-complete' )
 			)
+		);
+		self::$handout_file = array(
+			'name' => 'Handout File',
+			'id' => $this->prefix . 'handout-upload',
+			'type' => 'file'
 		);
 	}
 
@@ -70,8 +75,9 @@ class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 		?>
 		<div class="ctlt-events-row">
 			<div class="ctlt-span-12">
-				<label class="ctlt-span-4 ctlt-events-col" for="handout-upload">Handout File:</label>
-				<input class="ctlt-span-6 ctlt-events-col" type="file" name="handout-upload" id="handout-upload" />
+				<label class="ctlt-span-4 ctlt-events-col" for="<?php echo self::$handout_file['id']; ?>"><?php echo self::$handout_file['name']; ?>:</label>
+				<input class="ctlt-span-6 ctlt-events-col" type="<?php echo self::$handout_file['type']; ?>" name="<?php echo self::$handout_file['id']; ?>" id="<?php echo self::$handout_file['id']; ?>" />
+				<?php $this->add_download_link( $post->ID ); ?>
 			</div>
 		</div>
 		<?php
@@ -96,7 +102,8 @@ class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 		elseif( !current_user_can( 'edit_post', $post_id ) ) {
 			return $post_id;
 		}
-		// do the actual saving here
+
+		// saving the radios
 		$old_post_meta = get_post_meta( $post_id, self::$radios_arr['id'], true );
 		$new_post_meta = $_POST[self::$radios_arr['id']];
 		if( $new_post_meta && $new_post_meta != $old_post_meta ) {
@@ -106,6 +113,49 @@ class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 			delete_post_meta( $post_id, self::$radios_arr['id'], $old_post_meta );
 		}
 		
+		// saving the file upload
+		// http://wp.tutsplus.com/tutorials/attaching-files-to-your-posts-using-wordpress-custom-meta-boxes-part-1/
+		// make sure the file array isn't empty
+		if( !empty( $_FILES[self::$handout_file['id']]['name'] ) ) {
+			// setup the array of supported file types.
+			$supported_types = array( 'application/pdf' );
+
+			// get the file type of the upload
+			$arr_file_type = wp_check_filetype( basename( $_FILES[self::$handout_file['id']]['name'] ) );
+			$uploaded_type = $arr_file_type['type'];
+
+			// check if the type is supported. if not, throw an error
+			if( in_array( $uploaded_type, $supported_types ) ) {
+
+				// use the wordpress API to upload the file
+				$upload = wp_upload_bits( $_FILES[self::$handout_file['id']]['name'], null, file_get_contents( $_FILES[self::$handout_file['id']]['tmp_name'] ) );
+
+				if( isset( $upload['error'] ) && $upload['error'] != 0 ) {
+					wp_die( 'There was an error uploading your file. The error is: ' . $upload['error'] );
+				}
+				else {
+					update_post_meta( $post_id, self::$handout_file['id'], $upload );
+				}
+
+			}
+			else {
+				wp_die( "The file type that you've uploaded is not a PDF." );
+			}
+		}
+
+	}
+
+	public function update_edit_form() {
+		echo ' enctype="multipart/form-data"';
+	}
+
+	public function add_download_link( $post_id ) {
+		$attachment = get_post_meta( $post_id, self::$handout_file['id'], 'true' );
+		?>
+		<a class="ctlt-span-12 ctlt-events-col" href="<?php echo $attachment['url']; ?>" download="handout.pdf">
+			Download the attachment here
+		</a>
+		<?php
 	}
 
 }

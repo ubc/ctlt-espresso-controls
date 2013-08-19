19679d4f940b61e0f05354660ea07ad5bf4b2d9d
psmagicman <julienlaw1@gmail.com>
2013-08-19 13:53:37 -0700

    added a variable for the hook name

--- lib/class.ctlt-espresso-metaboxes.php
+++ lib/class.ctlt-espresso-metaboxes.php
@@ -2,10 +2,18 @@
 
 class CTLT_Espresso_Metaboxes {
 
-	public function __construct() {
+	protected $hook_name = 'ctlt_espresso_pre_advanced_options';
+
+	public static function render_boxes() {
+
+	}
+
+	public static function nonce_input() {
+
+	}
+
+	public static function events_meta_table() {
 
 	}
 
 }
-
-new CTLT_Espresso_Metaboxes();

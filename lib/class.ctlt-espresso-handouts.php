72615aa7bb4200feb51fd364d57a6cc8211c1375
psmagicman <julienlaw1@gmail.com>
2013-08-21 15:44:04 -0700

    can retrieve values saved to db now; uploads still don't work properly

--- lib/class.ctlt-espresso-handouts.php
+++ lib/class.ctlt-espresso-handouts.php
@@ -9,7 +9,7 @@ class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 		$this->init_handout_properties();
 		add_action( $this->add_hook, array( $this, 'handouts' ) );
 		add_action( $this->edit_hook, array( $this, 'handouts' ) );
-		add_action( 'post_edit_form_tag', array( $this, 'update_edit_form') );
+		add_action( 'ctlt_espresso_form', array( $this, 'update_edit_form') );
 	}
 
 	/**
@@ -24,10 +24,10 @@ class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 			'id' => self::$prefix . 'handouts_radio',
 			'type' => 'radio',
 			'options' => array(
-				array( 'name' => 'N/A', 'value' => 'n/a' ),
-				array( 'name' => 'Expected', 'value' => 'expected' ),
-				array( 'name' => 'Received', 'value' => 'received' ),
-				array( 'name' => 'Copying Complete', 'value' => 'copying-complete' )
+				array( 'name' => 'N/A', 'value' => 'N/A' ),
+				array( 'name' => 'Expected', 'value' => 'Expected' ),
+				array( 'name' => 'Received', 'value' => 'Received' ),
+				array( 'name' => 'Copying Complete', 'value' => 'Copying Complete' )
 				)
 			);
 		self::$handout_file = array(
@@ -53,6 +53,7 @@ class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 			<div class="inside">
 				<?php $this->the_radio_buttons(); ?>
 				<?php $this->the_file_upload(); ?>
+				<?php print_r( self::$data ); ?>
 			</div>
 		</div>
 		<?php
@@ -68,9 +69,9 @@ class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 			<div class="ctlt-span-12">
 				<label class="ctlt-inline ctlt-span-4 ctlt-events-col" for="<?php echo self::$radios_arr['id']; ?>"><?php echo self::$radios_arr['name']; ?></label>
 				<?php foreach( self::$radios_arr['options'] as $option ) { ?>
-					<?php //$checked = $meta == $option['value'] ? ' checked="checked"' : ''; ?>
+					<?php $checked = self::$data[self::$radios_arr['id']] == $option['value'] ? ' checked="checked"' : ''; ?>
 					<label class="ctlt-inline ctlt-span-2 ctlt-events-col">
-						<input type="<?php echo self::$radios_arr['type']; ?>" name="<?php echo self::$radios_arr['id']; ?>" value="<?php echo $option['name']; ?>" <?php //echo $checked; ?> /> <?php echo $option['name']; ?>
+						<input type="<?php echo self::$radios_arr['type']; ?>" name="<?php echo self::$radios_arr['id']; ?>" value="<?php echo $option['value']; ?>" <?php echo $checked; ?> /> <?php echo $option['name']; ?>
 					</label>
 				<?php } ?>
 			</div>
@@ -88,7 +89,7 @@ class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 			<div class="ctlt-span-12">
 				<label class="ctlt-span-4 ctlt-events-col" for="<?php echo self::$handout_file['id']; ?>"><?php echo self::$handout_file['name']; ?>:</label>
 				<input class="ctlt-span-6 ctlt-events-col" type="<?php echo self::$handout_file['type']; ?>" name="<?php echo self::$handout_file['id']; ?>" id="<?php echo self::$handout_file['id']; ?>" />
-				<?php //$this->add_download_link( $post->ID ); ?>
+				<?php //$this->add_download_link(); ?>
 			</div>
 		</div>
 		<?php
@@ -100,6 +101,20 @@ class CTLT_Espresso_Handouts extends CTLT_Espresso_Metaboxes {
 	 */
 	public function update_edit_form() {
 		echo ' enctype="multipart/form-data"';	
+	}
+
+	/**
+	 * add_download_link function
+	 * This function adds a download link to the uploaded file if it exists
+	 */
+	public function add_download_link() {
+		$attachment = self::$data[self::$handout_file['id']];
+		print_r( $attachment );
+		if( !empty( $attachment['url'] ) ) { ?>
+		<a class="ctlt-span-12 ctlt-events-col" href="<?php echo $attachment['url']; ?>" download="handout.pdf">
+			Download the attachment here
+		</a>
+		<?php }
 	}
 	
 }
